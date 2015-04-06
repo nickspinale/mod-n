11bfb57acfbbdec2495e0884b69c0e3053d72e74
Nick Spinale <spinalen@carleton.edu>
2015-04-05 23:50:34 -0500

    better interface

--- Data/BigWord.hs
+++ Data/BigWord.hs
@@ -8,7 +8,7 @@
 
 ---------------------------------------------------------
 -- |
--- Module      : Data.BigWord
+-- Module      : Data.W
 -- Copyright   : (c) 2015 Nick Spinale
 -- License     : MIT
 --
@@ -20,127 +20,164 @@
 ---------------------------------------------------------
 
 module Data.BigWord
-    ( BigWord
+    (
+    -- * The 'W' newtype
+      W
+    -- * Operations
     , (>+<)
-    , takeBE
-    , takeLE
-    , giveBE
-    , giveLE
+    , split
+
+    -- * Utility functions
+    , accumulate
+    , accumulate'
+    , chunks
+    , chunks'
+
     ) where
 
-import Control.Monad
 import Data.Bits
 import Data.Data
 import Data.Function
 import Data.Ix
-import Data.List
 import Data.Proxy
 import Data.Monoid
-import Data.Word (Word8)
+import Data.Traversable
 import GHC.TypeLits
 import Text.Printf
 
--- | Type representing a sequence of n bits, or a non-negative integer smaller than 2^n
-newtype BigWord (n :: Nat) = BigWord { getBigWord :: Integer }
+-- | Type representing a sequence of @n@ bits, or a non-negative integer smaller than @2^n@.
+newtype W (n :: Nat) = W { getW :: Integer }
     deriving (Eq, Ord, Real, Ix, PrintfArg, Data, Typeable)
 
-instance KnownNat n => Read (BigWord n) where
+-- Original name was BigWord, but since using this module requires more
+-- explicit type signatures, I decided to use just W. This may be stupid.
+-- Sorry if the name conflicts with your code.
+
+-------------------------------
+-- INSTANCES
+-------------------------------
+
+instance KnownNat n => Read (W n) where
     readsPrec = ((.).(.)) (map $ \(a, str) -> (fromInteger a, str)) readsPrec
 
-instance Show (BigWord n) where
-    show = show . getBigWord
+instance Show (W n) where
+    show = show . getW
 
-instance KnownNat n => Bounded (BigWord n) where
+instance KnownNat n => Bounded (W n) where
     minBound = 0
-    maxBound = BigWord (bit (natValInt (Proxy :: Proxy n)) - 1)
+    maxBound = W (bit (natValInt (Proxy :: Proxy n)) - 1)
 
 -- Can't just derive because it need to get the default for bounded types
-instance KnownNat n => Enum (BigWord n) where
-    toEnum = BigWord . toEnum
-    fromEnum = fromEnum . getBigWord
+instance KnownNat n => Enum (W n) where
+    toEnum = W . toEnum
+    fromEnum = fromEnum . getW
 
-instance KnownNat n => Integral (BigWord n) where
-    toInteger = getBigWord
-    quotRem x y = case (quotRem `on` getBigWord) x y of (q, r) -> (BigWord q, BigWord r)
+instance KnownNat n => Integral (W n) where
+    toInteger = getW
+    quotRem x y = case (quotRem `on` getW) x y of (q, r) -> (W q, W r)
 
-instance KnownNat n => Num (BigWord n) where
-    fromInteger = BigWord . flip mod (bit (natValInt (Proxy :: Proxy n)))
-    (+) = ((.).(.)) fromInteger ((+) `on` getBigWord)
-    (*) = ((.).(.)) fromInteger ((*) `on` getBigWord)
+instance KnownNat n => Num (W n) where
+    fromInteger = W . flip mod (bit (natValInt (Proxy :: Proxy n)))
+    (+) = ((.).(.)) fromInteger ((+) `on` getW)
+    (*) = ((.).(.)) fromInteger ((*) `on` getW)
     abs = id
     signum 0 = 0
     signum _ = 1
     negate = (+ 1) . complement
 
-instance KnownNat n => Bits (BigWord n) where
-        (.&.) = ((.).(.)) BigWord ((.&.) `on` getBigWord)
-        (.|.) = ((.).(.)) BigWord ((.|.) `on` getBigWord)
-        xor   = ((.).(.)) BigWord (xor   `on` getBigWord)
-        complement = fromInteger . complement . getBigWord
-        shift (BigWord x) i = BigWord $ shift x i `mod` bit (natValInt (Proxy :: Proxy n))
+instance KnownNat n => Bits (W n) where
+        (.&.) = ((.).(.)) W ((.&.) `on` getW)
+        (.|.) = ((.).(.)) W ((.|.) `on` getW)
+        xor   = ((.).(.)) W (xor   `on` getW)
+        complement = fromInteger . complement . getW
+        shift (W x) i = W $ shift x i `mod` bit (natValInt (Proxy :: Proxy n))
         rotate x i = let nat = natValInt (Proxy :: Proxy n)
                          dist = mod i nat
                      in shift x dist .|. shift x (nat - dist)
         bitSizeMaybe = Just . finiteBitSize
         bitSize = finiteBitSize
         isSigned = const False
-        testBit = testBit . getBigWord
+        testBit = testBit . getW
         bit i = if i < natValInt (Proxy :: Proxy n)
-                then BigWord (bit i)
+                then W (bit i)
                 else 0
-        popCount = popCount . getBigWord
+        popCount = popCount . getW
 
-instance KnownNat n => FiniteBits (BigWord n) where
+instance KnownNat n => FiniteBits (W n) where
     finiteBitSize = const $ natValInt (Proxy :: Proxy n)
 
--- | Appends two BigWords, treating the second's bits as more significant.
-(>+<) :: forall n m. (KnownNat n, KnownNat m, KnownNat (n + m)) => BigWord n -> BigWord m -> BigWord (n + m)
-(BigWord x) >+< (BigWord y) = fromInteger $ x + shift y (natValInt (Proxy :: Proxy m))
+-------------------------------
+-- EXPORTS
+-------------------------------
 
--- | Transforms an action that results in an octet to one that results in a BigWord of the specified size.
--- Octets resulting from the input action occur in network-byte order.
--- If n is not a multiple of 8, leftover bits are truncated.
-takeBE :: (Applicative f, KnownNat n) => f Word8 -> f (BigWord n)
-takeBE = takeAux mapAccumL
+-- | Appends two @'W'@'s, treating the second's bits as more significant.
+(>+<) :: forall n m. (KnownNat m, KnownNat n, KnownNat (m + n)) => W m -> W n -> W (m + n)
+(W x) >+< (W y) = fromInteger $ x + shift y (natValInt (Proxy :: Proxy m))
 
--- | Transforms an action that results in an octet to one that results in a BigWord of the specified size.
--- Octets resulting from the input action occur in little-endian order.
--- If n is not a multiple of 8, leftover bits are truncated.
-takeLE :: (Applicative f, KnownNat n) => f Word8 -> f (BigWord n)
-takeLE = takeAux mapAccumR
+-- | The inverse of @(>+<)@
+-- >    forall a b. split (a >+< b) == (a, b)
+split :: forall n m. (KnownNat m, KnownNat n, KnownNat (m + n)) => W (m + n) -> (W m, W n)
+split (W z) = (fromInteger z, fromInteger $ shiftR z (natValInt (Proxy :: Proxy m)))
 
-takeAux :: forall f n. (Applicative f, KnownNat n) =>
-    (  (Integer -> Word8 -> (Integer, Integer))
+-- | Transforms an applicative action that results in a @'W' n@ to one that results in a @'W' m@.
+-- @'W' n@'s are accumulated with significance increasing from left to right
+-- If m is not a multiple of n, leftover bits are truncated.
+--
+-- Example usage, parsing a little endian 160-bit word:
+--
+-- >    parseW160 = accumulate $ (fromIntegral :: Word8 -> W 8) <$> anyWord8
+accumulate :: (Applicative f, KnownNat m, KnownNat n) => f (W n) -> f (W m)
+accumulate = takeAux mapAccumR
+
+-- | Same as @accumulate@, but gathers in the opposite order
+accumulate' :: (Applicative f, KnownNat m, KnownNat n) => f (W n) -> f (W m)
+accumulate' = takeAux mapAccumL
+
+-- | Break a @'W' m@ into its constituent @'W' n@'s, and combine using the supplied monoid
+-- Chunks are appended with significance increasing from left to right.
+-- If n does not divide m, missing bits are treated as 0.
+--
+-- Example usage, building a little endian 160-bit word:
+--
+-- >    buildW160 = chunks' $ word8 . (fromIntegral :: W 8 -> Word8)
+chunks :: forall a m n. (Monoid a, KnownNat m, KnownNat n) => (W n -> a) -> W m -> a
+chunks = (.) getDual . chunks' . (.) Dual
+
+-- | Same as @chunks@, just appends in opposite order
+chunks' :: forall a m n. (Monoid a, KnownNat m, KnownNat n) => (W n -> a) -> W m -> a
+chunks' f = go (quot' m n) . toInteger
+  where
+    go 0 _ = mempty
+    go i x = f (fromInteger x) <> go (i - 1) (shiftR x n)
+    m = natValInt (Proxy :: Proxy m)
+    n = natValInt (Proxy :: Proxy n)
+
+-------------------------------
+-- HELPERS
+-------------------------------
+
+takeAux :: forall f n m. (Applicative f, KnownNat m, KnownNat n) =>
+    (  (Integer -> W n -> (Integer, Integer))
     -> Integer
-    -> [Word8]
+    -> [W n]
     -> (Integer, [Integer])
-    ) -> f Word8 -> f (BigWord n)
+    ) -> f (W n) -> f (W m)
 
 takeAux f = fmap ( fromInteger
                  . getSum
                  . foldMap Sum
                  . snd
-                 . f (\a b -> (a * 256, a * toInteger b)) 1
+                 . f (\a b -> (a * 2^n, a * toInteger b)) 1
                  )
           . sequenceA
-          . replicate (bytes (Proxy :: Proxy n))
-
--- | Break a BigWord into its constituent octets, and combine then as a monoid in network-byte order.
--- If n is not a multiple of 8, missing bits are treated as 0.
-giveBE :: forall m n. (Monoid m, KnownNat n) => (Word8 -> m) -> BigWord n -> m
-giveBE f = go (bytes (Proxy :: Proxy n)) . toInteger
+          . replicate (quot' m n)
   where
-    go 0 _ = mempty
-    go i n = f (fromInteger n) <> go (i - 1) (shiftR n 8)
-
--- | Break a BigWord into its constituent octets, and combine then as a monoid in little-endian order.
--- If n is not a multiple of 8, missing bits are treated as 0.
-giveLE :: (Monoid m, KnownNat n) => (Word8 -> m) -> BigWord n -> m
-giveLE = (.) getDual . giveBE . (.) Dual
+    m = natValInt (Proxy :: Proxy m)
+    n = natValInt (Proxy :: Proxy n)
 
 
-bytes :: KnownNat n => Proxy n -> Int
-bytes proxy = let (q, r) = natValInt proxy `quotRem` 8
+quot' :: Int -> Int -> Int
+quot' m n = let (q, r) = m `quotRem` n
               in case r of 0 -> q
                            _ -> q + 1
 
